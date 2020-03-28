ea760feeae2b0945772af0864148d5fff7117567
oldgit <jon.harvey.uk@gmail.com>
2020-03-28 23:23:37 +0000

    Initial version of petclinic generated by JHipster-6.8.0

---
+++ src/main/java/io/github/jhipster/petclinic/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package io.github.jhipster.petclinic.service.dto;
---
+++ src/test/java/io/github/jhipster/petclinic/cucumber/stepdefs/StepDefs.java
@@ -0,0 +1,9 @@
+package io.github.jhipster.petclinic.cucumber.stepdefs;
+
+import org.springframework.test.web.servlet.ResultActions;
+
+public abstract class StepDefs {
+
+    protected ResultActions actions;
+
+}
